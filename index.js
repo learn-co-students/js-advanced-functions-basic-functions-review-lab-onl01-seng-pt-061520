d2319e8308727b8defd405e00c3caf07a86fa978
rhday <54642787+rhday@users.noreply.github.com>
2021-01-12 12:28:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(style="*"){
+        return function(adjective="special"){
+            return `You are ${style}${adjective}${style}!` // this is wrapping "special" in "*" which when printed to the screen highlights/bolds "special" for us.
+        }
+}
+const Calculator = {
+    add: function(a,b) {
+        return a+b;
+    },
+    subtract: function(a,b) {
+        return a-b;
+    },
+    multiply: function(a,b) {
+        return a*b;
+    },
+    divide: function(a,b) {
+        return a/b;
+    },
+}
+function actionApplyer(start, functions) {
+    for (let i = 0; i < functions.length; i++) {
+        start = functions[i](start);
+    }
+    return start
+}
