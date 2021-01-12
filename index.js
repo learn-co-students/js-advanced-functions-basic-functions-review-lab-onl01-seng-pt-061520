e224e3154463fd9233b9f008a1b1ee41fbe05157
ajkelso <ajkelso@gmail.com>
2021-01-12 14:20:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+// saturdayFun()
+
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function (par="special") {
+        return `You are ${flair}${par}${flair}!`
+    }
+}
+
+let Calculator = {
+    add: function() {
+        return 1+3
+    },
+    subtract: function() {
+        return 1-3
+    },
+    multiply: function () {
+        return 1 * 3
+    },
+    divide: function() {
+        return 10 / 5
+    }
+
+}
+
+function actionApplyer(integer, array=[]) {
+    if (array === undefined || array.length == 0) {
+        return integer
+    }else{
+        let x = integer
+        array.forEach(fn => x = fn(x))
+        return x
+    }
+}
