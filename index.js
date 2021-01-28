f9ebc8db4132210e2315a0de53d817401c0eb859
Jade Lynn Masker <jadelynn@masker.family>
2021-01-28 10:24:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+function saturdayFun(thingToDo="roller-skate") {
+	return `This Saturday, I want to ${thingToDo}!`;
+}
+
+function mondayWork(workToDo="go to the office") {
+	return `This Monday, I will ${workToDo}.`;
+}
+
+function wrapAdjective(style="*") {
+	return function(adjective="special") {
+		return `You are ${style}${adjective}${style}!`;
+	};
+}
+
+const Calculator = {
+	add: function(a, b) {
+		return a + b;
+	},
+	subtract: function(a, b) {
+		return a - b;
+	},
+	multiply: function(a, b) {
+		return a * b;
+	},
+	divide: function(a, b) {
+		return a / b;
+	}
+};
+
+function actionApplyer(start, functions) {
+	let a = start;
+
+	for (let i = 0; i < functions.length; i++) {
+		a = functions[i](a);
+	}
+
+	return a;
+}
